86a82a803a5d115b219457a0cd50a8f111b57a05
ijustbsd <ijustbsd@gmail.com>
2017-04-12 13:24:31 +0300

    Format messages changed from string to JSON

--- static/js/chat.js
+++ static/js/chat.js
@@ -4,15 +4,13 @@ $("#user-menu p").html(nick);
 var ws = new WebSocket("ws://" + location.host + "/websocket");
 
 var clients = [];
+var msg = {};
 
 ws.onmessage = function (event) {
-    var i = event.data.indexOf(":");
-    var user = event.data.substr(0, i);
-    var text = event.data.substr(i + 1);
-
-    if (clients.indexOf(user) == -1) {
-        clients.push(user);
-        $("#dialogs-list ul").append("<li id='" + user + "' " + (nick == user ? " class='selected'" : "") + "><img src='https://lorempixel.com/54/54/cats/' alt='' class='avatar online'><p class='username'>" + user + "</p><p class='last-msg'>Ex aliquam optio quae odio.</p></li>");
+    msg = JSON.parse(event.data);
+    if (clients.indexOf(msg.user) == -1) {
+        clients.push(msg.user);
+        $("#dialogs-list ul").append("<li id='" + msg.user + "' " + (nick == msg.user ? " class='selected'" : "") + "><img src='https://lorempixel.com/54/54/cats/' alt='' class='avatar online'><p class='username'>" + msg.user + "</p><p class='last-msg'>Ex aliquam optio quae odio.</p></li>");
         $("#dialogs-list").stop().animate({
         scrollTop: $('#dialogs-list')[0].scrollHeight
     }, 800);
@@ -23,7 +21,9 @@ ws.onmessage = function (event) {
     var h = date.getHours();
     var m = date.getMinutes();
     var time = (h < 10 ? "0" + h : h) + ":" + (m < 10 ? "0" + m : m);
-    $("#messages-list ul").append("<li><div class='msg " + ((nick == user) ? '' : 'to-me') + "'><p class='text'><b>" + user +"</b>:<br />" + text + "</p><p class='time'>" + time + "</p></div></li>");
+    $("#messages-list ul").append("<li><div class='msg " + ((nick == msg.user) ? '' : 'to-me') + "'>\
+        <p class='text'><b>" + msg.user +"</b>:<br />" + msg.text + "</p>\
+        <p class='time'>" + time + "</p></div></li>");
     $("#messages-list").stop().animate({
         scrollTop: $('#messages-list')[0].scrollHeight
     }, 800);
@@ -35,8 +35,10 @@ $(document).keypress(function(event) {
 });
 
 $("#send-button").click(function(event) {
-    if (/\S/.test($("#input-msg").val()))
-        ws.send(nick + ":" + $("#input-msg").val());
+    msg['user'] = nick;
+    msg['text'] = $("#input-msg").val();
+    if (/\S/.test(msg.text))
+        ws.send(JSON.stringify(msg));
     $("#input-msg").val("");
     textAreaInit();
 });
